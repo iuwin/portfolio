f26942079576e9ae1a92efa2b7fba103d8e13212
Erwin Amper <erwinamper27@gmail.com>
2025-02-10 21:58:36 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
+
+
+AOS.init();
